1a19f4de8aa8977091e6342141379be701bb8236
fisam <pavlovilyaj1@gmail.com>
2026-07-22 17:07:48 +0500

    я сдохну щас

--- PromVesClient/Service/UserService/UserService.cs
+++ PromVesClient/Service/UserService/UserService.cs
@@ -52,20 +52,24 @@ namespace PromVesClient.Service.UserService
             
         }
         //метод создания пользователя
-        public async Task<ServiceResult> createUserAsync(string login, string password)
+        public async Task<ServiceResult> createUserAsync(string login, string password, string role)
         {
             //проверяет пустые ли строки лоигна и пароля
             if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
             {
                 return ServiceResult.Fail("Логин или пароль пустой");
             }
-            
+            //проверка есть ли уже такой пользователь
+            if (await UserExistsAsync(login))
+                return ServiceResult.Fail("Пользователь уже существует.");
+
             try
             {
                 var user = new User
                 {
                     Id = Guid.NewGuid(),
                     Name = login,
+                    Role = role,
                     PasswordHash = _hashPasswordService.getHashPasswordUser(password)
                 };
                 _dbContext.Users.Add(user);
@@ -92,6 +96,84 @@ namespace PromVesClient.Service.UserService
             }
 
             
+        }
+        //метод получения всех пользователей
+        public async Task<List<User>> GetUsersAsync()
+        {
+            return await _dbContext.Users
+                .OrderBy(u => u.Name)
+                .ToListAsync();
+        }
+        // проверка существования пользователя
+        public async Task<bool> UserExistsAsync(string login)
+        {
+            return await _dbContext.Users
+                .AnyAsync(u => u.Name == login);
+        }
+        //получения пользователя по id
+        public async Task<User?> GetUserAsync(Guid id)
+        {
+            return await _dbContext.Users
+                .FirstOrDefaultAsync(u => u.Id == id);
+        }
+        //метод удаления пользователя
+        public async Task<ServiceResult> DeleteUserAsync(Guid id)
+        {
+            try
+            {
+                var user = await _dbContext.Users.FindAsync(id);
+
+                if (user == null)
+                    return ServiceResult.Fail("Пользователь не найден.");
+
+                _dbContext.Users.Remove(user);
+
+                await _dbContext.SaveChangesAsync();
+
+                return ServiceResult.Ok();
+            }
+            catch (Exception ex)
+            {
+                return ServiceResult.Fail(ex.Message);
+            }
+        }
+        //изменение пользователя
+        public async Task<ServiceResult> UpdateUserAsync(
+    Guid id,
+    string login,
+    string password,
+    string role)
+        {
+            try
+            {
+                var user = await _dbContext.Users.FindAsync(id);
+
+                if (user == null)
+                    return ServiceResult.Fail("Пользователь не найден.");
+
+                var userWithSameName = await _dbContext.Users
+    .FirstOrDefaultAsync(u => u.Name == login && u.Id != id);
+
+                if (userWithSameName != null)
+                    return ServiceResult.Fail("Пользователь уже существует.");
+
+                user.Name = login;
+                user.Role = role;
+
+                if (!string.IsNullOrWhiteSpace(password))
+                {
+                    user.PasswordHash =
+                        _hashPasswordService.getHashPasswordUser(password);
+                }
+
+                await _dbContext.SaveChangesAsync();
+
+                return ServiceResult.Ok();
+            }
+            catch (Exception ex)
+            {
+                return ServiceResult.Fail(ex.Message);
+            }
         }
     }
 }
---
+++ PromVesClient/UserManagementForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PromVesClient
+{
+    partial class UserManagementForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvUser = new DataGridView();
+            dgvUserList = new DataGridView();
+            btnCreateUser = new Button();
+            btnEditUser = new Button();
+            btnDeleteUser = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvUser).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvUserList).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvUser
+            // 
+            dgvUser.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvUser.Location = new Point(12, 12);
+            dgvUser.Name = "dgvUser";
+            dgvUser.Size = new Size(621, 82);
+            dgvUser.TabIndex = 0;
+            // 
+            // dgvUserList
+            // 
+            dgvUserList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvUserList.Location = new Point(12, 122);
+            dgvUserList.Name = "dgvUserList";
+            dgvUserList.Size = new Size(621, 252);
+            dgvUserList.TabIndex = 1;
+            // 
+            // btnCreateUser
+            // 
+            btnCreateUser.Location = new Point(663, 22);
+            btnCreateUser.Name = "btnCreateUser";
+            btnCreateUser.Size = new Size(125, 61);
+            btnCreateUser.TabIndex = 2;
+            btnCreateUser.Text = "button1";
+            btnCreateUser.UseVisualStyleBackColor = true;
+            // 
+            // btnEditUser
+            // 
+            btnEditUser.Location = new Point(663, 153);
+            btnEditUser.Name = "btnEditUser";
+            btnEditUser.Size = new Size(125, 61);
+            btnEditUser.TabIndex = 3;
+            btnEditUser.Text = "button2";
+            btnEditUser.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteUser
+            // 
+            btnDeleteUser.Location = new Point(663, 289);
+            btnDeleteUser.Name = "btnDeleteUser";
+            btnDeleteUser.Size = new Size(125, 61);
+            btnDeleteUser.TabIndex = 4;
+            btnDeleteUser.Text = "button3";
+            btnDeleteUser.UseVisualStyleBackColor = true;
+            // 
+            // UserManagementForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 395);
+            Controls.Add(btnDeleteUser);
+            Controls.Add(btnEditUser);
+            Controls.Add(btnCreateUser);
+            Controls.Add(dgvUserList);
+            Controls.Add(dgvUser);
+            Name = "UserManagementForm";
+            Text = "UserForm";
+            ((System.ComponentModel.ISupportInitialize)dgvUser).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvUserList).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvUser;
+        private DataGridView dgvUserList;
+        private Button btnCreateUser;
+        private Button btnEditUser;
+        private Button btnDeleteUser;
+    }
+}
